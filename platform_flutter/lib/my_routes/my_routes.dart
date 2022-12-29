50e64a1598a8fec70b792bb2318c59797510b561
danganhphu <phudanganh107@gmail.com>
2022-12-29 15:54:39 +0700

    add routes

---
+++ platform_flutter/lib/my_routes/my_routes.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:platform_flutter/channel/channels.dart';
+
+class MyHomepage extends StatelessWidget {
+  const MyHomepage({Key? key}) : super(key: key);
+
+  Widget builButton(BuildContext context, {required String title, required Widget description}) =>  SizedBox(
+    width: 250,
+    child: ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 6.0),
+          backgroundColor: Colors.teal,
+          textStyle: const TextStyle(fontSize: 24)
+      ),
+      onPressed: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => description));
+      }, child: Text(title),
+    ),
+
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Demo MethodChannel"),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            buildPadding(),
+            builButton(context, title: "Battery", description: const MyBattery()),
+            buildPadding(),
+            builButton(context, title: "Device Info", description: const MyDeviceInfo()),
+            buildPadding(),
+            builButton(context, title: "Show Toast", description: const MyShowToast()),
+            buildPadding()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding buildPadding() {
+    return const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+            );
+  }
+}
