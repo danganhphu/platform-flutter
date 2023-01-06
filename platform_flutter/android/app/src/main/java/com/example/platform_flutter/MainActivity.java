bd4f5dedfe9e0a1af420905c147c32b8dbcd57d1
danganhphu <phudanganh107@gmail.com>
2023-01-06 17:10:55 +0700

    update handle navite

--- platform_flutter/android/app/src/main/java/com/example/platform_flutter/MainActivity.java
+++ platform_flutter/android/app/src/main/java/com/example/platform_flutter/MainActivity.java
@@ -20,20 +20,23 @@ import com.example.platform_flutter.platform_views_textview.NativeTextViewFactor
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 public class MainActivity extends FlutterActivity {
     private static final String CHANNELBATTERY = "com.example/battery";
     private static final String CHANNELDEVICEINFO = "com.example/deviceinfo";
-    private static final String CHANNELDEVICEINFO2 = "com.example/deviceinfo2";
     private static final String CHANNELANDROIDVERSION = "com.example/android_version";
+    private static final String CHANNEL_TOAST = "com.example/toast_channel";
+    private static final String CHANNEL_CALCULATE = "com.example/calculate_channel";
 
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         super.configureFlutterEngine(flutterEngine);
         demoMethodChannelBattery(flutterEngine);
+        demoToastMessage(flutterEngine);
+        demoCalculate(flutterEngine);
         demoMethodChannelDeviceInfo(flutterEngine);
-        demoMethodChannelDeviceInfo2(flutterEngine);
         demoMethodChannelAndroidVersion(flutterEngine);
         flutterEngine
                 .getPlatformViewsController()
@@ -63,17 +66,19 @@ public class MainActivity extends FlutterActivity {
     }
 
     public void demoMethodChannelDeviceInfo(@NonNull FlutterEngine flutterEngine) {
-        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNELDEVICEINFO)
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNELDEVICEINFO, JSONMethodCodec.INSTANCE)
                 .setMethodCallHandler(
                         (call, result) -> {
                             // This method is invoked on the main thread.
                             if (call.method.equals("getDeviceInfo")) {
-                                String deviceInfo = getDeviceInfo();
-                                if (!Objects.equals(deviceInfo, "Unknown")) {
-                                    result.success(deviceInfo);
+                                String type = call.argument("type");
+                                if (!(type == null || type.isEmpty())) {
+                                    result.success(deviceInfo(type));
                                 } else {
-                                    result.error("UNAVAILABLE", "DeviceInfo not available.", null);
+                                    result.error("ERROR", "type can not null", null);
                                 }
+
+
                             } else {
                                 result.notImplemented();
                             }
@@ -81,15 +86,15 @@ public class MainActivity extends FlutterActivity {
                 );
     }
 
-    public void demoMethodChannelDeviceInfo2(@NonNull FlutterEngine flutterEngine) {
-        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNELDEVICEINFO2, JSONMethodCodec.INSTANCE)
+    public void demoToastMessage(@NonNull FlutterEngine flutterEngine) {
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL_TOAST, JSONMethodCodec.INSTANCE)
                 .setMethodCallHandler(
                         (call, result) -> {
                             // This method is invoked on the main thread.
-                            if (call.method.equals("getDeviceInfo2")) {
+                            if (call.method.equals("getToastMessage")) {
                                 String type = call.argument("type");
                                 if (!(type == null || type.isEmpty())) {
-                                    result.success(getDeviceInfo2(type));
+                                    result.success(toastMessage(type));
                                 } else {
                                     result.error("ERROR", "type can not null", null);
                                 }
@@ -121,6 +126,36 @@ public class MainActivity extends FlutterActivity {
                 );
     }
 
+
+    public void demoCalculate(@NonNull FlutterEngine flutterEngine) {
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL_CALCULATE, JSONMethodCodec.INSTANCE)
+                .setMethodCallHandler(
+                        (call, result) -> {
+                            // This method is invoked on the main thread.
+                            if (call.method.equals("getCaculate")) {
+                                Integer num1 = call.argument("numberOne");
+                                Integer num2 = call.argument("numberTwo");
+                                Integer num3 = call.argument("numberThree");
+
+                                int numb1 = num1 != null ? num1 : 0;
+                                int numb2 = num2 != null ? num2 : 0;
+                                int numb3 = num3 != null ? num3 : 0;
+
+                               String kqua = giaiPTBac2((float) numb1, (float) numb2, (float) numb3);
+                                if (!kqua.isEmpty()) {
+                                    result.success(kqua);
+                                } else {
+                                    result.error("UNAVAILABLE", "Calculated not available.", null);
+                                }
+
+                            } else {
+                                result.notImplemented();
+                            }
+                        }
+                );
+    }
+
+
     private int getBatteryLevel() {
         int batteryLevel = -1;
         if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
@@ -136,18 +171,7 @@ public class MainActivity extends FlutterActivity {
         return batteryLevel;
     }
 
-    private String getDeviceInfo() {
-        String deviceInfo = "Unknown";
-        if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
-            deviceInfo = Build.MODEL;
-        } else {
-            deviceInfo = "Loi roi";
-        }
-
-        return deviceInfo;
-    }
-
-    private JSONObject getDeviceInfo2(String type) {
+    private JSONObject deviceInfo(String type) {
         JSONObject json = new JSONObject();
         if (type.equals("MODEL")) {
             try {
@@ -161,6 +185,22 @@ public class MainActivity extends FlutterActivity {
         return null;
     }
 
+    private JSONObject toastMessage(String type) {
+        JSONObject json = new JSONObject();
+        String msg = "Anh Phú";
+        if (type.equals("msg")) {
+            try {
+                json.put("result", msg);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return json;
+        }
+
+        return null;
+    }
+
+
     private String getAndroidVersion() {
         String androidVersion = "Unknown";
         if (VERSION.SDK_INT >= VERSION_CODES.LOLLIPOP) {
@@ -170,5 +210,23 @@ public class MainActivity extends FlutterActivity {
         }
 
         return androidVersion;
+    }
+
+    public static String giaiPTBac2(float a, float b, float c) {
+        float delta = (b*b)-(4*a*c);
+        float sqrt = (float) (Math.sqrt(delta));
+        String result;
+
+        if (delta > 0) {
+            float x1 = (-b + sqrt) / (2*a);
+            float x2 = (-b - sqrt) / (2*a);
+            result = "Pt 2 nghiệm là: " + x1 + " và " + x2;
+        } else if (delta == 0) {
+            float x1 = -b / (2 * a);
+            result = "Pt có nghiệm kép: " + "x1 = x2 = " + x1;
+        } else {
+            result = "Phương trình vô nghiệm";
+        }
+        return result;
     }
 }
